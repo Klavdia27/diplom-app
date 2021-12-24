aa2be216b0189757f0cd1aa3f71ced16e7bdd3a4
Klavdia Petukhova <kklavdia27@gmail.com>
2021-12-24 17:16:11 +0300

    add header for diplom app

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import Header from "./header/Header.jsx";
+
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      
+    
+    </div>
+  );
+}
+
+export default App;
